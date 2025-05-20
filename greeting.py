766fa04bfb3676a8e5517ce7fc9d8752480f7825
soumya18111 <soumyadeepmaity18@gmail.com>
2025-05-20 17:15:11 +0530

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -4,4 +4,4 @@ last_name = input("Enter your last name: ")
 
 full_name = first_name + " " + last_name
 
-print("Hello, " + full_name + "! Welcome!")
+print("Hello, " + full_name + "! Welcome to the python program")
