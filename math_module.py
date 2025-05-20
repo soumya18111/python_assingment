2ce79e6f3e7627f3d9dd553422588e298cf15dda
soumya18111 <soumyadeepmaity18@gmail.com>
2025-05-20 17:25:57 +0530

    Create math_module.py

---
+++ math_module.py
@@ -0,0 +1,12 @@
+import math
+
+number = float(input("Enter a positive number: "))
+
+sqrt_result = math.sqrt(number)
+log_result = math.log(number)
+sine_result = math.sin(number)
+
+
+print(f"Square root of {number} is: {sqrt_result}")
+print(f"Natural logarithm (ln) of {number} is: {log_result}")
+print(f"Sine of {number} radians is: {sine_result}")
